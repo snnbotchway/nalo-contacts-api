dff56557e45ab365facfb844b292adc0d049de7f
snnbotchway <solomonbotchway7@gmail.com>
2022-07-14 07:41:15 +0000

    fixed upload csv endpoint to gracefully catch errors in uploading empty and invalid csv files

--- api/views.py
+++ api/views.py
@@ -63,51 +63,55 @@ def contactDelete(request, pk):
 @permission_required('admin.can_add_log_entry')
 def uploadCsv(request):
     template = 'upload-csv.html'
+    try:
+        prompt = {
+            'order': 'Order of CSV should be name, phone'
+        }
 
-    prompt = {
-        'order': 'Order of CSV should be name, phone'
-    }
+        if request.method == 'GET':
+            return render(request, template, prompt)
 
-    if request.method == 'GET':
-        return render(request, template, prompt)
+        csv_file = request.FILES['file']
 
-    csv_file = request.FILES['file']
+        if not csv_file.name.endswith('.csv'):
+            messages.error(request, 'This is not a csv file')
+            return render(request, template)
 
-    if not csv_file.name.endswith('.csv'):
-        messages.error(request, 'This is not a csv file')
+        data_set = csv_file.read().decode('utf-8')
+        io_string = io.StringIO(data_set)
+        next(io_string)
 
-    data_set = csv_file.read().decode('utf-8')
-    io_string = io.StringIO(data_set)
-    next(io_string)
+        contact_list = []
+        for column in csv.reader(io_string, delimiter=',', quotechar='|'):
+            contact = Contact(
+                name = column[0],
+                phone = column[1]
+            )
 
-    contact_list = []
-    for column in csv.reader(io_string, delimiter=',', quotechar='|'):
-        contact = Contact(
-            name = column[0],
-            phone = column[1]
-        )
+            contact_list.append(contact)
+        
+        Contact.objects.bulk_create(contact_list)
+        if contact_list:
+            send_mail(
+                'CSV file uploaded',
+                'Your csv file was uploaded and the database was successfully populated with the information',
+                'solomonbotchway7@gmail.com',
+                [request.user.email],
+                fail_silently=True,
+            )
+            print('email sent')
+            print(request.user.email)
+            messages.success(request, 'Database successfully populated with csv file content')
+        else:
+            messages.error(request, 'Something went wrong. Try again')
+        contact_list = []
 
-        contact_list.append(contact)
-    
-    Contact.objects.bulk_create(contact_list)
-    if contact_list:
-        send_mail(
-            'CSV file uploaded',
-            'Your csv file was uploaded and the database was successfully populated with the information',
-            'solomonbotchway7@gmail.com',
-            [request.user.email],
-            fail_silently=False,
-        )
-        print('email sent')
-        print(request.user.email)
-        messages.success(request, 'Database successfully populated with csv file content')
-    else:
-        messages.error(request, 'Something went wrong. Try again')
-    contact_list = []
+        context= {}
 
-    context= {}
-
-    return render(request, template, context)
+        return render(request, template, context)
+    except:
+        messages.error(request, 'Something went wrong, make sure you have uploaded a valid csv file and try again')
+        return render(request, template)
 
     
 
